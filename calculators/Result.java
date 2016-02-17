e2271317e7c189aba16fd45c911600564baf7785
Pavel Osipenko <osipenko_pavel@ukr.net>
2016-02-17 17:44:47 +0200

    Пытаюсь написать свою реализаци калькуляторов

--- calculators/Result.java
+++ calculators/Result.java
@@ -3,12 +3,22 @@ package mytests.calculators;
 /**
  * Created by PavelOsipenko on 17.02.2016.
  */
+
+
 public class Result {
     public double acc; // Аккумулятор
-    public String rest; // остаток строки, которую мы еще не обработали
+    public String startPart; // начало строки, которую мы еще не обработали
+    public String lastPart; // остаток строки, которую мы еще не обработали
 
-    public Result(double v, String r) {
-        this.acc = v;
-        this.rest = r;
+    /**
+     *
+     * @param startPart - начало строки
+     * @param acc - текущее значение результата
+     * @param lastPart - оставшая строка
+     */
+    public Result(String startPart, double acc, String lastPart) {
+        this.acc = acc;
+        this.startPart = startPart;
+        this.lastPart = lastPart;
     }
 }
